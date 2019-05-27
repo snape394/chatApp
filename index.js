7453673d6e3a9aa8e721bdaccf4c661a872a4255
snape394 <47630393+snape394@users.noreply.github.com>
2019-05-27 14:55:21 +0530

    Update and rename app.js to index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var socket = require('socket.io');
 
 // App setup
 var app = express();
-var server = app.listen(4000, function(){
-    console.log('listening for requests on port 4000,');
+var server = app.listen(8080, function(){
+    console.log('listening for requests on port 8080,');
 });
 
 // Static files
